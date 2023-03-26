1795f81b890d08625b6feb5ce4dc3625d966adc1
DanielHo-BS <danielho1671@gmail.com>
2023-03-26 22:02:45 +0800

    Fix the error of JBF.py of HW1

--- HW1/hw1_material/part2/JBF.py
+++ HW1/hw1_material/part2/JBF.py
@@ -18,13 +18,13 @@ class Joint_bilateral_filter(object):
         ### TODO ###
         # Pre-compute
         h, w, ch = img.shape
-        output = np.zeros_like(img)
+        output = np.zeros_like(img).astype(np.float32)
         scaleFactor_s = 1 / (2 * (self.sigma_s ** 2))
         scaleFactor_r = 1 / (2 * (self.sigma_r ** 2))
         # A lookup table for range kernel
         LUT = np.exp(-np.arange(256) / 255 * np.arange(256) / 255 * scaleFactor_r)
         # A spatial Gaussian function
-        x ,y = np.meshgrid(self.wndw_size - self.pad_w , self.wndw_size - self.pad_w)
+        x ,y = np.meshgrid(np.arange(self.wndw_size) - self.pad_w , np.arange(self.wndw_size) - self.pad_w)
         kernel_s = np.exp(-(x ** 2 + y ** 2) * scaleFactor_s)
         
         # Main
@@ -43,11 +43,11 @@ class Joint_bilateral_filter(object):
             for y in range(self.pad_w, self.pad_w + h):
                 for x in range(self.pad_w, self.pad_w + w):
                     wgt = LUT[abs(padded_guidance[y - self.pad_w:y + self.pad_w + 1, x - self.pad_w:x + self.pad_w + 1] - padded_guidance[y, x])] * kernel_s
-                    wacc = wgt / np.sum(wgt)
-                    output[y - self.pad_w, x - self.pad_w, 0] = np.sum(wacc * padded_img[y - self.pad_w:y + self.pad_w + 1, x - self.pad_w:x + self.pad_w + 1, 0])
-                    output[y - self.pad_w, x - self.pad_w, 1] = np.sum(wacc * padded_img[y - self.pad_w:y + self.pad_w + 1, x - self.pad_w:x + self.pad_w + 1, 1])
-                    output[y - self.pad_w, x - self.pad_w, 2] = np.sum(wacc * padded_img[y - self.pad_w:y + self.pad_w + 1, x - self.pad_w:x + self.pad_w + 1, 2])
+                    wacc = np.sum(wgt)
+                    output[y - self.pad_w, x - self.pad_w, 0] = np.sum(wgt * padded_img[y - self.pad_w:y + self.pad_w + 1, x - self.pad_w:x + self.pad_w + 1, 0]) / wacc
+                    output[y - self.pad_w, x - self.pad_w, 1] = np.sum(wgt * padded_img[y - self.pad_w:y + self.pad_w + 1, x - self.pad_w:x + self.pad_w + 1, 1]) / wacc
+                    output[y - self.pad_w, x - self.pad_w, 2] = np.sum(wgt * padded_img[y - self.pad_w:y + self.pad_w + 1, x - self.pad_w:x + self.pad_w + 1, 2]) / wacc
         else:
             print('Error ndim of image or guidance!!!')
-        print(output[100,100,:])
+        #print(output[100,100,:].astype(np.uint8))
         return np.clip(output, 0, 255).astype(np.uint8)
