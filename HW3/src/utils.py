4eb3a38814af2b6391024bb501acbd8db33abc90
DanielHo-BS <danielho1671@gmail.com>
2023-04-28 13:41:20 +0800

    Updatep part1 of HW3

--- HW3/src/utils.py
+++ HW3/src/utils.py
@@ -19,9 +19,14 @@ def solve_homography(u, v):
         print('At least 4 points should be given')
 
     # TODO: 1.forming A
-
+    A = np.zeros((2*N,9))
+    for i in range(N):
+        A[i*2,:] = [u[i][0], u[i][1], 1, 0, 0, 0, -u[i][0]*v[i][0], -u[i][1]*v[i][0], -v[i][0]]
+        A[i*2+1,:] = [0, 0, 0, u[i][0], u[i][1], 1, -u[i][0]*v[i][1], -u[i][1]*v[i][1], -v[i][1]]
     # TODO: 2.solve H with A
-
+    [U, S, V] = np.linalg.svd(A)
+    h = V.T[:,-1]
+    H = np.reshape(h,(3,3))
     return H
 
 
@@ -63,29 +68,50 @@ def warping(src, dst, H, ymin, ymax, xmin, xmax, direction='b'):
     H_inv = np.linalg.inv(H)
 
     # TODO: 1.meshgrid the (x,y) coordinate pairs
-
+    Ux, Uy = np.meshgrid(range(xmin,xmax), range(ymin,ymax))
     # TODO: 2.reshape the destination pixels as N x 3 homogeneous coordinate
+    U = np.concatenate(([Ux.reshape(-1)], [Uy.reshape(-1)],
+                        [np.ones((xmax-xmin) * (ymax-ymin))]), axis=0)
 
     if direction == 'b':
-        # TODO: 3.apply H_inv to the destination pixels and retrieve (u,v) pixels, then reshape to (ymax-ymin),(xmax-xmin)
-
+        # TODO: 3-1.apply H_inv to the destination pixels and retrieve (u,v) pixels
+        H_inv = np.linalg.inv(H)
+        V = np.dot(H_inv, U)
+        Vx, Vy = V/V[2]
+        # TODO: 3-2.then reshape to (ymax-ymin),(xmax-xmin)
+        Vx = Vx.reshape(ymax-ymin, xmax-xmin)
+        Vy = Vy.reshape(ymax-ymin, xmax-xmin)
         # TODO: 4.calculate the mask of the transformed coordinate (should not exceed the boundaries of source image)
-
-        # TODO: 5.sample the source image with the masked and reshaped transformed coordinates
-
+        mask = (((Vx<w_src-1) & (0<=Vx)) & ((Vy<h_src-1) & (0<=Vy)))
+        # TODO: 5-1.sample the source image with the masked
+        mVx = Vx[mask]
+        mVy = Vy[mask]
+        # TODO: 5-2.interpolation and reshaped transformed coordinates
+        mVxi = mVx.astype(int)
+        mVyi = mVy.astype(int)
+        dX = (mVx- mVxi).reshape((-1,-1))
+        dY = (mVy- mVyi).reshape((-1,-1))
+        p = np.zeros(h_src, w_src, ch)
+        p[mVyi, mVxi, :] += (1-dY)*(1-dX)*src[mVyi, mVxi, :]
+        p[mVyi, mVxi, :] += (dY)*(1-dX)*src[mVyi+1, mVxi, :]
+        p[mVyi, mVxi, :] += (1-dY)*(dX)*src[mVyi, mVxi+1, :]
+        p[mVyi, mVxi, :] += (dY)*(dX)*src[mVyi+1, mVxi+1, :]
         # TODO: 6. assign to destination image with proper masking
-
-        pass
+        dst[ymin:ymax,xmin:xmax][mask] = p[mVyi,mVxi]
 
     elif direction == 'f':
-        # TODO: 3.apply H to the source pixels and retrieve (u,v) pixels, then reshape to (ymax-ymin),(xmax-xmin)
-
+        # TODO: 3-1.apply H to the source pixels and retrieve (u,v) pixels
+        V = np.dot(H,U)
+        V = (V/V[2]).astype(int)
+        # TODO: 3-2.then reshape to (ymax-ymin),(xmax-xmin)
+        Vx = V[0].reshape(ymax-ymin, xmax-xmin)
+        Vy = V[1].reshape(ymax-ymin, xmax-xmin)
         # TODO: 4.calculate the mask of the transformed coordinate (should not exceed the boundaries of destination image)
-
+        mask = ((Vx<w_dst)&(0<=Vx))&((Vy<h_dst)&(0<=Vy))
         # TODO: 5.filter the valid coordinates using previous obtained mask
-
+        mVx = Vx[mask]
+        mVy = Vy[mask]
         # TODO: 6. assign to destination image using advanced array indicing
-
-        pass
+        dst[mVy, mVx, :] = src[mask]
 
     return dst
